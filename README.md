081880cd050e0ed4d7801d291b828a9f971ac929
Ruth Moog <ruthmoog@gmail.com>
2022-03-28 11:02:31 +0100

    Chapter 2: Degenerate Objects

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ Walkthrough of the book by Kent Beck
 - $5 + 10 CHF = £10 if rate is 2:1
 - ~~$5 * 2 = $10~~
 - Make `amount` private
-- Dollar side-effects?
+- ~~Dollar side-effects?~~
 - Money rounding?
--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -5,7 +5,7 @@ public class Dollar {
         this.amount = amount;
     }
 
-    public void times(int multiplier) {
-        amount *= multiplier;
+    public Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
     }
 }
