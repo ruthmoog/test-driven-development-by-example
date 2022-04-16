92ff7a7654d5662573e53d4288421a90065d2bcd
Ruth Moog <ruthmoog@gmail.com>
2022-04-16 15:04:47 +0100

    Chapter 6: Equality for All, Redux

---
+++ src/main/java/Money.java
@@ -0,0 +1,8 @@
+class Money {
+    protected int amount;
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount;
+    }
+}
--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -3,21 +3,33 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class DollarTest {
+class MoneyTest {
 
     @Test
-    @DisplayName("Multiply two numbers")
+    @DisplayName("Multiply two dollar amounts")
     public void testMultiplication() {
         Dollar five = new Dollar(5);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
 
+
+    @Test
+    @DisplayName("Multiply two franc amounts")
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
+
+
     @Test
     @DisplayName("Two like numbers are equal")
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
 
     }
 }
